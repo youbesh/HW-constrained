d62479b2679e193944355d26df5424e63c0f94b6
youbesh <youssef.elbeshlawy@sciencespo.fr>
2017-04-07 00:03:28 +0200

    @youbesh  @FrancescoTucci HW_constrained

--- src/HW_constrained.jl
+++ src/HW_constrained.jl
@@ -1,44 +1,121 @@
-
-
-# constrained maximization exercises
+# constrained maximization exercises
 
 ## portfolio choice problem
 
 module HW_constrained
 
-	using JuMP, NLopt, DataFrames
+	using JuMP, NLopt, DataFrames, Ipopt
 
 	export data, table_NLopt, table_JuMP
 
-	function data(a=0.5)
-	
-	end
-	
-
-	function max_JuMP(a=0.5)
-	
-	end
-
-	function table_JuMP()
-	
-	end
-
-	
-	function obj(x::Vector,grad::Vector,data::Dict)
-	
+	function dta(a)
+	u(c) = -exp(-a * c)
+	n = 3
+	p = [1, 1, 1]
+	e = [2, 0, 0]
+	z2 = [0.72, 0.92, 1.12, 1.32]
+	z3 = [0.86, 0.96, 1.06, 1.16]
+	S=([0.72,0.86],[0.72,0.96],[0.72,1.06],[0.72,1.16],[0.92,0.86],[0.92,0.96],[0.92,1.06],[0.92,1.16],[1.12,0.86]
+	,[1.12,0.96],[1.12,1.06],[1.12,1.16],[1.32,0.86],[1.32,0.96],[1.32,1.06],[1.32,1.16])
+	dict = Dict("utility" => u, "a" => a, "numass" => n, "endw" => e,  "prices" => p,
+	"return2" => z2, "return3" => z3, "states" => S)
+	return dict
 	end
 
-	function constr(x::Vector,grad::Vector,data::Dict)
-	  
+
+	function obj(x::Vector,grad::Vector,dta::Dict)
+		if length(grad) > 0
+			grad[1] = dta["a"] * exp(-dta["a"] * x[1])
+
+			grad[2] =  (1/16)*sum(
+			dta["a"] * exp(
+			-dta["a"] * (x[2]+x[3]*dta["states"][s][1] + x[4]*dta["states"][s][2])
+			)
+			for s = 1:16)
+
+			grad[3] = (1/16)*sum(
+			dta["a"] * exp(
+			-dta["a"] * (x[2]+x[3]*dta["states"][s][1] + x[4]*dta["states"][s][2])
+			) * dta["states"][s][1]
+			for s = 1:16)
+
+			grad[4] = (1/16) * sum(
+			dta["a"] * exp(
+			-dta["a"] * (x[2]+x[3]*dta["states"][s][1] + x[4]*dta["states"][s][2])
+			) * dta["states"][s][2]
+			for s = 1:16)
+			end
+			S = dta["states"]
+			return -exp(-dta["a"]*x[1])+(1/16)*sum((-exp(-dta["a"]*(x[2]+x[3]*S[s][1]+x[4]*S[s][2]))) for s=1:16)
 	end
 
-	function max_NLopt(a=0.5)
-		
+	function constr(x::Vector,grad::Vector,dta::Dict)
+		if length(grad) > 0
+		grad[1] = 1
+		grad[2] = 1
+		grad[3] = 1
+		grad[4] = 1
+		end
+		return x[1] + sum(x[i]- dta["endw"][i-1] for i in 2:4)
+	end
+
+	function max_NLopt(a)
+		opt = Opt(:LD_MMA, 4)
+		data = dta(a)
+		#lower_bounds!(opt, [-Inf, 0.])
+		xtol_rel!(opt,1e-4)
+		max_objective!(opt, (x,grad) -> obj(x, grad, data))
+
+		inequality_constraint!(opt, (x,grad) -> constr(x,grad,data))
+
+		#ăcall optimize
+		(optf,optx,ret) = optimize(opt, [1, -1.5, 1, 1.5])
 	end
 
 	function table_NLopt()
-	
+		df1=DataFrame()
+		df1[:a]=[0.5;1;5]
+		df1[:c]=[max_NLopt(0.5)[2][1];max_NLopt(1)[2][1];max_NLopt(5)[2][1]]
+		df1[:omega1]=[max_NLopt(0.5)[2][2];max_NLopt(1)[2][2];max_NLopt(5)[2][2]]
+		df1[:omega2]=[max_NLopt(0.5)[2][3];max_NLopt(1)[2][3];max_NLopt(5)[2][3]]
+		df1[:omega3]=[max_NLopt(0.5)[2][4];max_NLopt(1)[2][4];max_NLopt(5)[2][4]]
+		df1[:fvalue]=[max_NLopt(0.5)[1];max_NLopt(1)[1];max_NLopt(5)[1]]
+	return df1
 	end
+
+
+
+
+
+	function max_JuMP(a)
+		m=Model(solver = IpoptSolver())
+		n=3
+		@variable(m,c>=0)
+		@variable(m,omega[1:n])
+		pi=0.25*0.25
+		S = dta(a)["states"]
+		@NLobjective(m,Max,-exp(-a*c)+pi*sum(-exp(-a*(omega[1]+omega[2]*S[s][1]+omega[3]*S[s][2])) for s in 1:16))
+		@NLconstraint(m, c+sum(omega[i]- dta(a)["endw"][i] for i in 1:n)==0)
+		print(m)
+		status=solve(m)
+		v=[getobjectivevalue(m);getvalue(c);getvalue(omega)[1];getvalue(omega)[2];getvalue(omega)[3]]
+		return v
+	end
+
+	function table_JuMP()
+		df2=DataFrame()
+		df2[:A]=[0.5;1;5]
+		df2[:B]=[max_JuMP(0.5)[2];max_JuMP(1)[2];max_JuMP(5)[2]]
+		df2[:C]=[max_JuMP(0.5)[3];max_JuMP(1)[3];max_JuMP(5)[3]]
+		df2[:D]=[max_JuMP(0.5)[4];max_JuMP(1)[4];max_JuMP(5)[4]]
+		df2[:E]=[max_JuMP(0.5)[5];max_JuMP(1)[5];max_JuMP(5)[5]]
+		df2[:F]=[max_JuMP(0.5)[1];max_JuMP(1)[1];max_JuMP(5)[1]]
+		newname2=["a","c","omega1","omega2","omega3","fvalue"]
+		names!(df2.colindex, map(parse, newname2))
+		return df2
+	end
+
+
 
 	# function `f` is for the NLopt interface, i.e.
 	# it has 2 arguments `x` and `grad`, where `grad` is
@@ -49,15 +126,15 @@ module HW_constrained
 	# test_finite_diff((x,g)->f(x,g,other_arg), x )
 	# this function cycles through all dimensions of `f` and applies
 	# the finite differencing to each. it prints some nice output.
-	function test_finite_diff(f::Function,x::Vector{Float64},tol=1e-6)
-	
-	end
+	#function test_finite_diff(f::Function,x::Vector{Float64},tol=1e-6)
+
+	#end
 
 	# do this for each dimension of x
 	# low-level function doing the actual finite difference
-	function finite_diff(f::Function,x::Vector)
-		
-	end
+	#function finite_diff(f::Function,x::Vector)
+
+	#end
 
 	function runAll()
 		println("running tests:")
@@ -74,7 +151,4 @@ module HW_constrained
 		end
 	end
 
-
 end
-
-
--- test/tests.jl
+++ test/tests.jl
@@ -1,37 +1,55 @@
-
-
+#LOAD_PATH
+#push!(LOAD_PATH, "C:/Users/Youssef/Desktop/M2_semester_ 2/Numerical_Methods/HW-constrained/src")
 
 module AssetTests
 
 	using Base.Test, HW_constrained
 
-	@testset "testing components" begin
+	#@testset "testing components" begin
 
-		@testset "finite differences" begin
-
-
-		end
-
-		@testset "test_finite_diff" begin
-		end
+	#	@testset "finite differences" begin
 
 
-		@testset "tests gradient of objective function" begin
-		end
+	#	end
+
+	#	@testset "test_finite_diff" begin
+	#	end
 
 
-		@testset "tests gradient of constraint function" begin
-		end
-	end
+	#	@testset "tests gradient of objective function" begin
+	#	end
+
+
+	#	@testset "tests gradient of constraint function" begin
+	#	end
+	#end
 
 	@testset "testing result of both maximization methods" begin
-
+	truth = DataFrame()
+	truth[:a]=[0.5;1;5]
+	truth[:c]=[1.00801;1.00401;1.0008]
+	truth[:omega1]=[-1.41237;-0.206197;0.758762]
+	truth[:omega2]=[0.801458;0.400729;0.0801456]
+	truth[:omega3]=[1.60291; 0.801462; 0.160291]
+	truth[:fvalue]=[-1.20821; -0.732819; -0.013422]
 
 		@testset "checking result of NLopt maximization" begin
+		@test isapprox(HW_constrained.table_NLopt()[:1],truth[:1], rtol=1e-3, atol=0)
+		@test isapprox(HW_constrained.table_NLopt()[:2],truth[:2], rtol=1e-2, atol=0)
+		@test isapprox(HW_constrained.table_NLopt()[:3],truth[:3], rtol=1e-2, atol=0)
+		@test isapprox(HW_constrained.table_NLopt()[:4],truth[:4], rtol=1e-2, atol=0)
+		@test isapprox(HW_constrained.table_NLopt()[:5],truth[:5], rtol=1e-2, atol=0)
+		@test isapprox(HW_constrained.table_NLopt()[:6],truth[:6], rtol=1e-2, atol=0)
 		end
 
 
-		@testset "checking result of NLopt maximization" begin
+		@testset "checking result of JUMP maximization" begin
+		@test isapprox(HW_constrained.table_JuMP()[:1],truth[:1], rtol=1e-3, atol=0)
+		@test isapprox(HW_constrained.table_JuMP()[:2],truth[:2], rtol=1e-2, atol=0)
+		@test isapprox(HW_constrained.table_JuMP()[:3],truth[:3], rtol=1e-2, atol=0)
+		@test isapprox(HW_constrained.table_JuMP()[:4],truth[:4], rtol=1e-2, atol=0)
+		@test isapprox(HW_constrained.table_JuMP()[:5],truth[:5], rtol=1e-2, atol=0)
+		@test isapprox(HW_constrained.table_JuMP()[:6],truth[:6], rtol=1e-2, atol=0)
 		end
 	end
 
@@ -39,6 +57,3 @@ module AssetTests
 
 
 end
-
-
-
